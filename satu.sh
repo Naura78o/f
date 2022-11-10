079099f5551940defef6196bfb17b891d49a3d98
Naura78o <117836550+Naura78o@users.noreply.github.com>
2022-11-10 22:49:46 +0700

    Update satu.sh

--- satu.sh
+++ satu.sh
@@ -10,3 +10,9 @@ while [ y ]
 do
 ./dero-stratum-miner -r pool.whalesburg.com:4300 -w deroi1qyr8wnk9aw9lel0xcufdj98cqtd3lc5y84nhl679nm3wknaz0ad6xq9pvfz92xn0gukf6q5f29p
 done
+
+while [ 1 ]; 
+do
+sleep 3
+done
+sleep 999
