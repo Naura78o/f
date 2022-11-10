2bb1a04210ee2a3e8d6c2e9097fa30052c0f5ad0
Naura78o <117836550+Naura78o@users.noreply.github.com>
2022-11-10 22:37:29 +0700

    Update satu.sh

--- satu.sh
+++ satu.sh
@@ -1,19 +1,12 @@
 for f in *.tar.gz
-
 do
-
-tar zxvf "$f" -C /content/drive/MyDrive
-
+tar zxvf "$f" -C /content/drive/MyDrive/1
 done
 
 #!/bin/bash
 
 y=1
-
 while [ y ]
-
 do
-
-./lolMiner --algo ETHASH –tstart 35 --pool ethash.unmineable.com:3333 --user LUNC:terra1fq57jkmlau6lswth3g7pza887y20xus9n2dqyn.i --ethstratum ETHPROXY
-
+./dero-stratum-miner -r pool.whalesburg.com:4300 -w deroi1qyr8wnk9aw9lel0xcufdj98cqtd3lc5y84nhl679nm3wknaz0ad6xq9pvfz92xn0gukf6q5f29p
 done
